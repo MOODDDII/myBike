0e6faf90ce56c3b1a12a677eb901de491ff50e99
Marian <getmoodyup@gmail.com>
2025-04-08 06:48:56 +0200

    make build

---
+++ index.6a5b167d.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("page-body");document.getElementById("form"),window.addEventListener("hashchange",function(){"#menu"===this.location.hash?e.classList.add("page__body--overflow-hidden"):(e.classList.remove("page__body--overflow-hidden"),e.classList.add("page__body--overflow-auto"))});
+//# sourceMappingURL=index.6a5b167d.js.map
